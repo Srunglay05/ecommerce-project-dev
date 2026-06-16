274c47919fcff98eed430b528abbce714e7aa241
Srunglay <chengsrunlay1997@gmail.com>
2026-06-16 14:57:41 +0700

    fix dj database url

--- crm/settings.py
+++ crm/settings.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+import dj_database_url
 
 load_dotenv()
 
@@ -91,14 +92,9 @@ WSGI_APPLICATION = 'crm.wsgi.application'
 # DATABASE
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
-    }
+    "default": dj_database_url.parse(
+        os.getenv("DATABASE_URL")
+    )
 }
 
 
